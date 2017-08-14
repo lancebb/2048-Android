0c61ab16896e1dbc982da14e664a75c32b7124fc
lance <lance@buddybuild.com>
2017-08-14 12:57:22 -0700

    Update buddybuild_prebuild.sh
    changed to 30 min sleep

--- 2048/buddybuild_prebuild.sh
+++ 2048/buddybuild_prebuild.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-while :
+
+me=$$
+
+# Start a background process to signal us when the time has passed
+(
+        sleep 1800
+        kill -USR1 $me
+) &
+
+# When we recieve SIGUSR1, commit suicide
+trap "kill $me" SIGUSR1
+
+# Do stuff until we commit suicide
+while [ 1 -eq 1 ]
 do
-	sleep 1
+        sleep 1
 done
